69a81aa8af94e04ba0b36d49500129acc265f821
Dakota A. Justin <megatenkota2@gmail.com>
2015-10-02 10:45:38 -0600

    Creating Physician Class

---
+++ app/src/main/java/kirasoftstudios/physicianlist/Physician.java
@@ -0,0 +1,38 @@
+package kirasoftstudios.physicianlist;
+
+/**
+ * Created by Kota on 10/2/2015.
+ */
+
+
+public class Physician {
+
+    //</summary>
+    //Class to define phsyician
+    //</summary>
+
+    private String name;
+
+    private String specialty;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecialty() {
+        return specialty;
+    }
+
+    public void setSpecialty(String specialty) {
+        this.specialty = specialty;
+    }
+
+
+
+
+
+}
